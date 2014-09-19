7e9bc4eed07e9281e423da0e55fe6386dd49e7c3
Unknown author <unknown@unknown>
2014-09-19 00:00:00 +0000

    version 1.9-50

--- matlab/p_sPCAgrid_PostProc.m
+++
@@ -1,7 +0,0 @@
-function [ ret ] = p_sPCAgrid_PostProc (x, ret_C)
-
-	ret = p_PCAgrid_PostProc (x, ret_C) ;
-    
-    lambda_ini = p_sPCAgrid_GetLambda_ini (x) ;
-
-	ret.lambda = [lambda_ini, x.lambda] ;
